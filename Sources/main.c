18b9a25df50247af5f0a5353c744ecb58e56c183
MATTHIAS-PC\Matthias <matthias.schwegler@stud.hslu.ch>
2016-09-29 15:12:24 +0200

    richtig

--- Sources/main.c
+++ Sources/main.c
@@ -48,7 +48,7 @@ int main(void)
   /*** End of Processor Expert internal initialization.                    ***/
 
   /* Write your code here */
-  /* For example: for(;;) { sali michi } */
+  /* For example: for(;;) { sali } */
 
   /*** Don't write any code pass this line, or it will be deleted during code generation. ***/
   /*** RTOS startup code. Macro PEX_RTOS_START is defined by the RTOS component. DON'T MODIFY THIS CODE!!! ***/
