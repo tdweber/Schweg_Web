f34f9e912b7c88ee3e328e1fb73c4a2dda6d0c56
MATTHIAS-PC\Matthias <matthias.schwegler@stud.hslu.ch>
2016-09-29 15:09:05 +0200

    test

--- README.md
+++
@@ -1,2 +0,0 @@
-# Schweg_Web
-Roboter Gruppe
--- Test_robot/Debug/Project_Settings/Startup_Code/startup.d
+++
@@ -1,8 +0,0 @@
-Project_Settings/Startup_Code/startup.o: \
- ../Project_Settings/Startup_Code/startup.c \
- C:/Users/Michi/INTRO_projects/Test_robot/Generated_Code/IO_Map.h \
- C:/Users/Michi/INTRO_projects/Test_robot/Static_Code/IO_Map/MK22F12.h
-
-C:/Users/Michi/INTRO_projects/Test_robot/Generated_Code/IO_Map.h:
-
-C:/Users/Michi/INTRO_projects/Test_robot/Static_Code/IO_Map/MK22F12.h:
